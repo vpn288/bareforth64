dc58b65b7c15d9cb7199a3174556247c0b8b6589
vpn288 <vpn@vden.ru>
2014-09-04 17:40:04 +0400

    Update forth.asm

--- forth.asm
+++ forth.asm
@@ -1,1 +1,22 @@
-ret
+
+
+[BITS 64]
+[ORG 0x0000000000200000]
+
+%INCLUDE "bmdev.asm"
+
+start:	
+	mov	r13,[start+rax+r8]	
+	ret	
+	
+	_pop:
+        mov eax , [ r7 + r8 ]
+        sub r7 , 8
+	and r7 , r9
+	ret
+	    
+  _push:
+        add r7 , 8
+        and r7 , r9
+        mov [ r7 + r8 ] , eax
+        ret
