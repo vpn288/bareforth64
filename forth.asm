10fd444e84901512bff84280af2d0063ee263582
vpn288 <vpn@vden.ru>
2014-09-05 16:46:28 +0400

    Update forth.asm

--- forth.asm
+++ forth.asm
@@ -1,4 +1,3 @@
-
 [BITS 64]
 [ORG 0x0000000000200000]
 %INCLUDE "bmdev.asm"
@@ -6,10 +5,21 @@ start:
 	mov	r8,[data_stack_base]
 	xor	r10, r10
 	mov	r9,[data_stack_mask]
+
 	mov	rax,nfa_0
 	call	_push
+	call	_count
 	call	_type
+	mov	rax,0x123456789abcdef0
+	call	rax_to_hex
+	mov	[numbr],rbx
+	mov	[numbr+8],rdx
+	mov	rsi,numbr
+	mov	rcx,16
+	call	b_output
 	ret	
+
+
 _pop:
 	mov rax , [ r10 + r8 ]
 	sub r10 , 8
@@ -32,6 +42,7 @@ _type:
 	mov	rsi,rax
 	call	b_output_chars
 	ret
+
 _count:
 	call	_pop
 	mov	cl,[rax]
@@ -42,9 +53,32 @@ _count:
 	call	_push
 	ret
 
+low_nibble_of_al_to_hex:
+	; bl - result
+	and	al,0x0f
+	add	al,0x30
+	mov	bl,al
+	ret
+	
+rax_to_hex:
+	;result in rdx,rbx
+	mov	rcx,16
+	rol	rax,4
+	rth:
+	push	rax
+	shld	rdx,rbx,8
+	call	low_nibble_of_al_to_hex
+	pop	rax
+	rol	rax,4
+	loop	rth
+	ret
+
 ;-----------------------
 data_stack_base	dq	0x300000
 data_stack_mask	dq	0x0fffff
+
+numbr	dq	0,0
+	db	0
 nfa_0:
 db 7, "FORTH64" ;
 db 0 ; oa?ieie?o?ua-au?aaieaa?uea ioee
