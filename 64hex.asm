c7ffcbfd322ae50e0a5cb8a87fdaade5f1148128
vpn288 <vpn@vden.ru>
2014-09-12 16:14:42 +0400

    Update 64hex.asm

--- 64hex.asm
+++ 64hex.asm
@@ -2,11 +2,17 @@
 ; [value]
 ;
 
-        vmovq   xmm0,[value]
-        vmovq   xmm1,xmm0
-        andps   xmm0,[mask] ;pand?
-        paddb   xmm0,[adds]
         
-        and     rax,0f0f0f0f0f0f0f0f
-        and     rbx,f0f0f0f0f0f0f0f0
+        mov     rax,[value]
+        mov     rbx,rax
         
+        and     rax,0f0f0f0f0f0f0f0fh
+        and     rbx,f0f0f0f0f0f0f0f0h
+        
+        shr     rbx,4
+        imul    rax,1f1f1f1f1f1f1f1fh
+        imul    rbx,1f1f1f1f1f1f1f1fh
+        shr     rax,4
+        shr     rbx,4
+        add     rax,3030303030303030h
+        add     rbx,3030303030303030h
