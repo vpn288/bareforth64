d7ba329d26aa107552f65c02f4d1de67e23f1219
vpn288 <vpn@vden.ru>
2014-10-07 09:43:31 +0400

    Update atoi.asm

--- atoi.asm
+++ atoi.asm
@@ -1,4 +1,4 @@
-  mov	rax,[hexstr]
+	mov	rax,[hexstr]
 	bswap	rax
 	mov	rbx,[hexstr+8]
 	bswap	rbx
@@ -11,13 +11,13 @@
 	movdqu	xmm3,[sixes]
 	movdqu	xmm4,[zeroes]
 	movdqu	xmm7,[bytemask]
-	psubb	xmm0,xmm4	; ????? ????
-	paddb	xmm0,xmm3	; ???? ?????
-	movdqa	xmm5,xmm0	;
+	psubb	xmm0,xmm4	; sub zeroes
+	paddb	xmm0,xmm3	; 
+	movdqa	xmm5,xmm0	; getting carrys for A-F
 	pand	xmm0,xmm2	
-	psubb	xmm0,xmm3	;????? ?????
+	psubb	xmm0,xmm3	;
 	psrlq	xmm5,4
-	pand	xmm5,xmm2	;???????? ?????? ????????
+	pand	xmm5,xmm2	;
 	paddb	xmm0,xmm5
 	psllq	xmm5,3
 	por	xmm0,xmm5
@@ -31,7 +31,7 @@
 	
 	packsswb	xmm0,xmm8
 	packsswb	xmm6,xmm8
-	psllq	xmm6,4
+	psllq		xmm6,4
 	por		xmm0,xmm6
 
 	movdqu	[value],xmm0
@@ -42,11 +42,14 @@ value     dq  0
 	
 hexstr    db '1234567890ABCDEF'
 
-efes:	    dq	0f0f0f0f0f0f0f0fh
-	        dq	0f0f0f0f0f0f0f0fh
+efes:	  dq	0f0f0f0f0f0f0f0fh
+	  dq	0f0f0f0f0f0f0f0fh
+
 zeroes:	  dq	3030303030303030h
-	        dq	3030303030303030h
+	  dq	3030303030303030h
+	  
 sixes:	  dq	0606060606060606h
-	        dq	0606060606060606h
+	  dq	0606060606060606h
+	  
 bytemask  dq	0ff00ff00ff00ffh
-			    dq	0ff00ff00ff00ffh	      
+	  dq	0ff00ff00ff00ffh	      
